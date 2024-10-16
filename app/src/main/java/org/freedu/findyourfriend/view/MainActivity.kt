c88ec6883b2ae4ae4370f4e58dbc8a706b930dae
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-10-16 08:58:10 +0600

    Authentication and user data save

---
+++ app/src/main/java/org/freedu/findyourfriend/view/MainActivity.kt
@@ -0,0 +1,72 @@
+package org.freedu.findyourfriend.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import org.freedu.findyourfriend.R
+import org.freedu.findyourfriend.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    lateinit var actionDrawerToggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        var navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomBar.setupWithNavController(navController)
+        binding.drawerNav.setupWithNavController(navController)
+
+        actionDrawerToggle = ActionBarDrawerToggle(
+            this, binding.drawerlayout,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        actionDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.drawerNav.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.logout -> {
+                    Firebase.auth.signOut()
+                    startActivity(
+                        Intent(this, LoginActivity::class.java)
+                    )
+                    finish()
+                }
+
+            }
+            true
+        }
+        binding.bottomBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.logout -> {
+                    Firebase.auth.signOut()
+                    startActivity(
+                        Intent(this, LoginActivity::class.java)
+                    )
+                    finish()
+                }
+
+            }
+            true
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+}
